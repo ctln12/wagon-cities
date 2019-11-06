e31da2ec4ca8c89290a2fde198ead6e3e42b46e1
Cataline Baert <snuf_kin@yahoo.com>
2019-11-06 21:06:14 +0100

    prepare citiesReducer to handle some actions

---
+++ src/reducers/cities_reducer.jsx
@@ -0,0 +1,10 @@
+const citiesReducer = (state, action) => {
+  if (state === undefined) {
+    // Reducer initialisation
+    return [];
+  }
+
+  // TODO: handle some actions
+};
+
+export default citiesReducer;
