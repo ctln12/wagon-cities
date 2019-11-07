eee358b113271b2d38b5975393283c5f682c62cf
Cataline Baert <snuf_kin@yahoo.com>
2019-11-07 11:29:29 +0100

    rename function correctly, set condition if no active city, use correct props and refactor image source

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
-const App = () => {
+import { connect } from 'react-redux';
+
+const ActiveCity = (props) => {
+  if (!props.activeCity) {
+    return (
+      <div className="active-city">
+        <p>Select a city...</p>
+      </div>
+    );
+  }
+
+  const url = `https://kitt.lewagon.com/placeholder/cities/${props.activeCity.slug}`;
+
   return (
     <div className="active-city">
-      <h1>Paris</h1>
-      <p>16 Villa Gaudelet, 75011 Paris</p>
-      <img src="https://kitt.lewagon.com/placeholder/cities/paris" alt="" />
+      <h3>{props.activeCity.name}</h3>
+      <p>{props.activeCity.address}</p>
+      <img src={url} width="100%" alt="" />
     </div>
   );
 };
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    activeCity: state.activeCity
+  };
+}
+
+export default connect(mapStateToProps)(ActiveCity);
