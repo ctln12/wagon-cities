af02d20c49c5e7842f25a5ce6bf581145a665e86
Cataline Baert <snuf_kin@yahoo.com>
2019-11-07 11:24:44 +0100

    modify mapStateToProps return statement, refactor dispatch return statement and h3 class

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -5,22 +5,29 @@ import { connect } from 'react-redux';
 import { setActiveCity } from '../actions/index';
 
 const City = (props) => {
+  const handleClick = () => {
+    props.setActiveCity(props.city);
+  };
   const { city } = props;
+  let classes = "list-group-item";
+  if (props.city === props.activeCity) {
+    classes += " active-city";
+  }
   return (
-    <h3 className="list-group-item">{city.name}</h3>
+    <h3 className={classes} onClick={handleClick}>{city.name}</h3>
   );
 };
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
-    { setActiveCity: setActiveCity },
+    { setActiveCity },
     dispatch
   );
 }
 
 function mapStateToProps(state) {
   return {
-    activeCity: state.city
+    activeCity: state.activeCity
   };
 }
 
