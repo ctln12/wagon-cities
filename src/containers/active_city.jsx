9e58c7ea1fc86028d308d675b539f000f712efaa
Cataline Baert <snuf_kin@yahoo.com>
2019-11-07 09:34:00 +0100

    create new component

---
+++ src/containers/active_city.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="active-city">
+      <h1>Paris</h1>
+      <p>16 Villa Gaudelet, 75011 Paris</p>
+      <img src="https://kitt.lewagon.com/placeholder/cities/paris" alt="" />
+    </div>
+  );
+};
+
+export default App;
